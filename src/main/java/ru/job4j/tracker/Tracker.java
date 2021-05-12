cccd76c879cd541a7b9ac14429fcdbe5aeb41989
Andrey Plakhotin <plahotin94@yandex.ru>
2021-05-13 01:22:45 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #62523]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,4 +60,18 @@ public class Tracker {
         }
         return false;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            int start = index + 1;
+            int distPos = index;
+            int length = size - index;
+            System.arraycopy(items, start, items, distPos, length);
+            items[size - 1] = null;
+            size--;
+            return true;
+        }
+        return false;
+    }
 }
