bb634af5fab00e82cb9863897cd78b1ea9480e0c
Andrey Plakhotin <plahotin94@yandex.ru>
2021-05-13 00:15:33 +0300

    5. Tracker - хранилище [#396 #62527]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,16 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String date = item.getCreated().format(formatter);
-        System.out.println("date: " + date);
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("First item"));
+        tracker.add(new Item("Second item"));
+        tracker.add(new Item("Third item"));
+        Item firstItem = tracker.findById(1);
+        Item secondItem = tracker.findById(2);
+        Item thirdItem = tracker.findById(3);
+        System.out.println(firstItem.getName());
+        System.out.println(secondItem.getName());
+        System.out.println(thirdItem.getName());
     }
 }
