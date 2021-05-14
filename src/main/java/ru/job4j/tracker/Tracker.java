0fbeca584cc664e8c91503d2a6609439c35f0923
Andrey Plakhotin <plahotin94@yandex.ru>
2021-05-14 07:49:52 +0300

    5. Tracker - хранилище [#521594] Fix

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -25,14 +25,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                rsl[count++] = items[i];
-            }
-        }
-        return Arrays.copyOf(rsl, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
