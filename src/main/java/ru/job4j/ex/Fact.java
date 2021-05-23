2ed24618e284bc8049db99fa6e242c095d8794ec
Andrey Plakhotin <plahotin94@yandex.ru>
2021-05-23 21:20:47 +0300

    0.3. Тестирование исключений с junit [#219366]

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -8,7 +8,7 @@ public class Fact {
 
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("argument cannot be less than 0");
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {
