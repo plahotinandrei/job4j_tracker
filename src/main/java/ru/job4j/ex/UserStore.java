3e7029564ac3539439629940e58ec25633a6af41
Andrey Plakhotin <plahotin94@yandex.ru>
2021-05-25 10:43:45 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #160369] fix

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -7,6 +7,7 @@ public class UserStore {
         for (User user:users) {
             if (login.equals(user.getUsername())) {
                 rsl = user;
+                break;
             }
         }
         if (rsl == null) {
