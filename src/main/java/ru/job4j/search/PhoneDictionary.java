da94d7513045f48318d7c3d94838190cf1bd33ce
Andrey Plakhotin <plahotin94@yandex.ru>
2021-06-14 01:14:12 +0300

    1. Телефонный справочник на базе ArrayList [#41598] fix

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -18,10 +18,10 @@ public class PhoneDictionary {
         ArrayList<Person> result = new ArrayList<>();
         for (Person person:persons) {
             if (
-                    person.getName().contains(key)
-                            || person.getSurname().contains(key)
-                            || person.getAddress().contains(key)
-                            || person.getPhone().contains(key)
+                person.getName().contains(key)
+                || person.getSurname().contains(key)
+                || person.getAddress().contains(key)
+                || person.getPhone().contains(key)
             ) {
                 result.add(person);
             }
