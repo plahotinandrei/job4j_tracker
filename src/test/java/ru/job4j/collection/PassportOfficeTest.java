3ab7eed1117c019aea567dbb801c0580b2fceab0
Andrey Plakhotin <plahotin94@yandex.ru>
2021-06-26 00:39:14 +0300

    1. Паспорт и жители [#212656 #171194] fix

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -14,4 +14,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void addDoubleCitizen() {
+        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivan Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen1);
+        assertThat(office.add(citizen2), is(false));
+    }
 }
