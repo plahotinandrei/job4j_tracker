58ed33a7072031788b6767b740007d5c0907966f
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2022-02-26 13:52:49 +0300

    2. Список адресов [#110062 #268817] fix

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,13 +7,13 @@ public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map((p) -> p.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
         return profiles.stream()
-                .map((p) -> p.getAddress())
+                .map(Profile::getAddress)
                 .sorted((a1, a2) -> a1.getCity().compareTo(a2.getCity()))
                 .distinct()
                 .collect(Collectors.toList());
